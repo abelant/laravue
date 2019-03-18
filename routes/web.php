71ca5304f19fd04efafc2126f4b0440e581685da
Abelant Zairi <abelant@outlook.com>
2019-03-18 15:19:35 +0100

    2nd Commit

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::get('/{any}', function () {
     return view('post');
 })->where('any', '.*');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
